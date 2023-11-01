7fe50680bf75666f0de8cbb3fb61d4d1a9cfde4b
Dmytro <149473021+myeisDmytroNam@users.noreply.github.com>
2023-11-01 20:44:45 +0200

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,9 @@
+Num1 = 8
+Num2 = 4
+
+print(Num1+Num2)
+print(Num1-Num2)
+print(Num1*Num2)
+print(type(Num1//Num2), Num1/Num2)
+print(type(Num1/Num2), Num1/Num2)
+# Решил использовать как / так и //
