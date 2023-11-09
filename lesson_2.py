5af4386ad5444554a17d379a040fcb31aa39dc74
DimaPB <dima.shvets.work@gmail.com>
2023-11-10 00:38:16 +0200

    Started extra task 2, get the the right answer to equasion

--- lesson_2.py
+++ lesson_2.py
@@ -64,14 +64,21 @@ print(a, b)
 # Памятайте що там є декілька випадків по іксам. Завдання не складне але обьємне.
 # Подумайте як прийняти дані від користувача, ну і над рештою).
 
-# x**2-3x-4=0
+# # x**2-3x-4=0
+#   1   -3 -4
+# d = "5*x"
+# c = d - "x"
 #
+# print(c)
+
 #
-# x = int(None)
-
-a = int(input("Enter the a: "))
-b = int(input("Enter the b: "))
-c = int(input("Enter the c: "))
-
-equation = a + b + c
-print(equation)
+a = int(input("Enter a: "))  # 1
+b = int(input("Enter b: "))  # -3
+c = int(input("Enter c: "))  # -4
+#
+# # d = b**2-4*a*c
+d_1 = ((-b+(b**2-4*a*c)**(1/2))//2*a)
+d_2 = ((-b-(b**2-4*a*c)**(1/2))//2*a)
+# # # d_1 = 3 + (9 + 16)**(1/2)
+#
+print(d_1, d_2)
