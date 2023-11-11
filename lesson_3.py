1d84939978c0437e2b7acbdf655ba27ebcc2c9fb
DimaPB <dima.shvets.work@gmail.com>
2023-11-11 21:19:26 +0200

    Task #1 is added

--- lesson_3.py
+++ lesson_3.py
@@ -1,15 +1,54 @@
-# Уявимо що до нас прийшов наш друг або подруга і попросили написати додаток для покупок в магазині
-# що б можна було скласти список продуктів та коли купуєш викреслювати
-# (для викреслення питайте номер елемента, що б видалити по індексу треба його передати в
-# pop list_a.pop(0) - видалить нульовий індекс, памятайте що користувач не знає що в нас
-# індекси починаються з нуля)
-# Також нам відомо що цей список має завжди пять елементів.
-# Після кожного видалення елементу виводьте наш оновлений список.
-# Після 5 видалень перевірте чи список пустий якщо не пустий напишіть що ось ще є продукти, якщо пусти то напишіть про це.
-# Також після цього кроку запропонуйте користувачу написати ще продуктів та додати його в кошик. і виведіть їх на екран. Але цього разу вже без видалень.
+# Task_1
 # кроки:
 # Спочатку пропонуємо користувачу ввести продукти.
 # Робимо 5 запитів на видалення
 # Перевіряємо корзину
 # Пропонуємо додати продукти
 # Виводмо список і прощаємось
+
+product_list = input("Enter the list of products:")
+product_list = product_list.replace(" ", "")
+product_list = product_list.split(",")
+elem_numb = len(product_list)
+
+if elem_numb > 0:
+    element_to_delete = int(input("Enter the number of product, you want to delete: ")) - 1
+    product_list.pop(element_to_delete)
+    elem_numb = elem_numb - 1
+    print(product_list, "\n")
+
+if elem_numb > 0:
+    element_to_delete = int(input("Enter the number of product, you want to delete: ")) - 1
+    product_list.pop(element_to_delete)
+    elem_numb = elem_numb - 1
+    print(product_list, "\n")
+
+if elem_numb > 0:
+    element_to_delete = int(input("Enter the number of product, you want to delete: ")) - 1
+    product_list.pop(element_to_delete)
+    elem_numb = elem_numb - 1
+    print(product_list, "\n")
+
+if elem_numb > 0:
+    element_to_delete = int(input("Enter the number of product, you want to delete: ")) - 1
+    product_list.pop(element_to_delete)
+    elem_numb = elem_numb - 1
+    print(product_list, "\n")
+
+if elem_numb > 0:
+    element_to_delete = int(input("Enter the number of product, you want to delete: ")) - 1
+    product_list.pop(element_to_delete)
+    elem_numb = elem_numb - 1
+    print(product_list, "\n")
+
+if elem_numb == 0:
+    print("The list is empty\n")
+else:
+    print("The list is not empty\n")
+
+new_product_list = input("Enter the list of products:")
+new_product_list = new_product_list.replace(" ", "")
+new_product_list = product_list + new_product_list.split(",")
+print(f"Your list {new_product_list}")
+
+# apple,juice,rice,tofu,chips,cola
