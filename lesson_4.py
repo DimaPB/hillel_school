a2ed2fd64bead44520b588ce14a2317af5749a8e
DimaPB <dima.shvets.work@gmail.com>
2023-11-16 21:40:17 +0200

    added task 1

--- lesson_4.py
+++ lesson_4.py
@@ -6,7 +6,35 @@
 # потім віднімаєте суму або відсоток відповідно від ціни яку отримали раніше і пишете користувачу кінцеву суму.
 # * завдання з зірочкою не впливає на бал. Округліть суму якщо копійок більше ніж 44 то це буде + 1 гривня,
 # якщо менше. то тоді просто відкидаємо копійки від ціни.
-#
+
+import decimal
+print("Enter the prices using spaces between them")
+price = input("Enter the prices: ")
+price = price.split()
+
+the_sum = 0
+for i in price:
+    the_sum = the_sum + int(i)
+    # i += i не понимаю почему тут с і ничего не надо делать, оно само берет следущее значение в массиве
+the_sum = the_sum * (1 - 6.5/100)
+discount = input("Do you have an discount? ").lower()
+if discount == "yes":
+    type_of_discount = input("Which one do ypu have? sum or %? ").lower()
+    if type_of_discount == "sum":
+        discount_amount = int(input("Enter your discount "))
+        the_sum = the_sum - discount_amount
+    elif type_of_discount == "%":
+        discount_amount = float(input("Enter your discount "))
+        the_sum = the_sum * (1 - discount_amount/100)
+    else:
+        print("Incorrect type of discount")
+rounded_sum = the_sum.__round__(0)
+if the_sum - rounded_sum >= 0.44:
+    the_sum =+ 1
+    print(f"The sum is {the_sum.__round__(0)}")
+else:
+    print(f"The sum is {the_sum.__round__(0)}")
+
 # Задача 2
 # Переробіть задачу з попереднього уроку враховуючи ваші знання з цього уроку, використайте цикл for in.
 #
@@ -29,7 +57,6 @@ while i < 3:
     else:
         print("Your card is blocked")
 
-
 #
 # Задача 4
 # Напишіть за допомогою f-string та format. Дві стрічки де буде підставлятись імя та вік з зміних
@@ -40,11 +67,3 @@ while i < 3:
 # print(f_string)
 # print(format_string)
 
-# Задача 4
-# Напишіть за допомогою f-string та format. Дві стрічки де буде підставлятись імя та вік з зміних
-# name = "оЛенА"
-# age = 21
-# f_string = тут ваш код формата ф-стрінг. | повино вийти -> Я Олена, мені 21 рік
-# format_string = тут ваш код формата формат стрінг | повино вийти -> Я Олена, мені 21 рік
-# print(f_string)
-# print(format_string)
