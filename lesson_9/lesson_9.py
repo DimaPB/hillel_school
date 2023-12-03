2b2f482b20a868583a05b66a9809b17591624c16
DimaPB <dima.shvets.work@gmail.com>
2023-12-03 20:44:01 +0200

    HW from lsseon 9 added

--- lesson_9/lesson_9.py
+++ lesson_9/lesson_9.py
@@ -4,3 +4,27 @@
 # 3) зробіть функцію як ми робили з додаванням
 # тільки замість двох чисел зробіть три числа і протестуйте її всіма трьома методами
 # 4) перепишіть декоратор який заміряє час з уроку в домашку, можете його поклацати, як він працює
+
+
+# def list_check(the_list):
+#     list_sum = 0
+#     try:
+#         for i in the_list:
+#             list_sum = list_sum + i
+#         print(list_sum)
+#     except(TypeError):
+#         print("There is an unexpected type in the list")
+#
+#     return list_sum
+#
+#
+# list_params = [1, 2, 3,]  # [1, 2, "3"]
+# list_check(list_params)
+
+
+def add_tree_numbers(number_1: int | float, number_2: int | float, number_3: int | float) -> int | float:
+    result = number_1 + number_2 + number_3
+    return result
+
+
+# add_two_numbers(1, 2,3)
---
+++ lesson_9/test_lesson_9.py
@@ -0,0 +1,37 @@
+import pytest
+import datetime
+from lesson_9 import add_tree_numbers
+
+
+#  solution 1
+# def test_1():
+#     assert add_tree_numbers(1, 2, 3) == 6
+#
+#
+# def test_2():
+#     assert add_tree_numbers(-1, -2, -3) == -6
+#
+#
+# def test_3():
+#     assert add_tree_numbers(1.8, 2.2, 3) == 7
+
+
+# solution 2
+
+@pytest.mark.parametrize("number_1, number_2, number_3, result", [
+    pytest.param(1, 2, 3, 6),
+    pytest.param(-1, -2, -3, -6),
+    pytest.param(1.2, 2.8, 3, 7.0)])
+def test_set_1(number_1, number_2, number_3, result):
+    assert add_tree_numbers(number_1, number_2, number_3) == result
+
+
+# solution 3
+
+test_list = [(1, 2, 3, 6), (-1, -2, -3, -6), (1.8, 2.2, 3, 7)]
+
+
+@pytest.mark.parametrize("number_1, number_2, number_3, result", test_list)
+def test_3(number_1, number_2, number_3, result):
+    assert add_tree_numbers(number_1, number_2, number_3) == result
+
