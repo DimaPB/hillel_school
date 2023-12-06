046acf02fa4eb4805c5f02f0f9c2bee4f0630959
DimaPB <dima.shvets.work@gmail.com>
2023-12-06 10:07:55 +0200

    Finished HW 9

--- lesson_9/lesson_9.py
+++ lesson_9/lesson_9.py
@@ -5,21 +5,34 @@
 # тільки замість двох чисел зробіть три числа і протестуйте її всіма трьома методами
 # 4) перепишіть декоратор який заміряє час з уроку в домашку, можете його поклацати, як він працює
 
+from datetime import datetime
 
-# def list_check(the_list):
-#     list_sum = 0
-#     try:
-#         for i in the_list:
-#             list_sum = list_sum + i
-#         print(list_sum)
-#     except(TypeError):
-#         print("There is an unexpected type in the list")
-#
-#     return list_sum
-#
-#
-# list_params = [1, 2, 3,]  # [1, 2, "3"]
-# list_check(list_params)
+
+def time_counter(func):
+    def counter(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        delta_time = datetime.now() - start
+        print(delta_time)
+        return result
+    return counter
+
+
+@time_counter
+def list_check(the_list):
+    list_sum = 0
+    try:
+        for i in the_list:
+            list_sum = list_sum + i
+        print(list_sum)
+    except(TypeError):
+        print("There is an unexpected type in the list")
+
+    return list_sum
+
+
+list_params = [1, 2, 3,]  # [1, 2, "3"]
+list_check(list_params)
 
 
 def add_tree_numbers(number_1: int | float, number_2: int | float, number_3: int | float) -> int | float:
@@ -28,3 +41,7 @@ def add_tree_numbers(number_1: int | float, number_2: int | float, number_3: int
 
 
 # add_two_numbers(1, 2,3)
+
+
+
+
--- lesson_9/test_lesson_9.py
+++ lesson_9/test_lesson_9.py
@@ -1,19 +1,19 @@
 import pytest
-import datetime
 from lesson_9 import add_tree_numbers
 
 
-#  solution 1
-# def test_1():
-#     assert add_tree_numbers(1, 2, 3) == 6
-#
-#
-# def test_2():
-#     assert add_tree_numbers(-1, -2, -3) == -6
-#
-#
-# def test_3():
-#     assert add_tree_numbers(1.8, 2.2, 3) == 7
+# solution 1
+
+def test_1():
+    assert add_tree_numbers(1, 2, 3) == 6
+
+
+def test_2():
+    assert add_tree_numbers(-1, -2, -3) == -6
+
+
+def test_3():
+    assert add_tree_numbers(1.8, 2.2, 3) == 7
 
 
 # solution 2
@@ -34,4 +34,5 @@ test_list = [(1, 2, 3, 6), (-1, -2, -3, -6), (1.8, 2.2, 3, 7)]
 @pytest.mark.parametrize("number_1, number_2, number_3, result", test_list)
 def test_3(number_1, number_2, number_3, result):
     assert add_tree_numbers(number_1, number_2, number_3) == result
+
 
