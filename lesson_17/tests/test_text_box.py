9c221dddcf1f0f40df395f95baf83f0e366e51b0
DimaPB <dima.shvets.work@gmail.com>
2024-01-28 18:03:42 +0200

    lesson 17 new branch

--- lesson_17/tests/test_text_box.py
+++ lesson_17/tests/test_text_box.py
@@ -1,4 +1,3 @@
-import time
 from lesson_17.TextBoxPage import TextBoxPage
 
 
@@ -6,18 +5,14 @@ class TestTextBox:
     def test_username_fill_and_check(self, chrome):
         page = TextBoxPage(chrome)
         page.open()
-        time.sleep(2)
         page.fill_full_name_field("Dimon")
-        time.sleep(3)
 
     def test_email_fill_and_check(self, chrome):
         page = TextBoxPage(chrome)
         page.open()
         page.fill_email_filed("@email.com")
-        time.sleep(3)
 
     def test_current_area_field(self, chrome):
         page = TextBoxPage(chrome)
         page.open()
         page.fill_current_area_field("Baker Street 221b")
-        time.sleep(3)
