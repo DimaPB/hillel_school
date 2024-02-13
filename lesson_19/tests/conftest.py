eabb3fd1c5494ef5c2ea08d3d8233b3a8de5792b
DimaPB <dima.shvets.work@gmail.com>
2024-02-14 00:30:20 +0200

    empty

--- lesson_19/tests/conftest.py
+++ lesson_19/tests/conftest.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 
 @pytest.fixture
 def chrome():
-    driver = webdriver.Chrome(executable_path="/home/dima/PycharmProjects/Lessons/hillel_school/lesson_18/chromedriver")
+    driver = webdriver.Chrome(executable_path="/home/user/PycharmProjects/Lessons/hillel_school/lesson_19/chromedriver")
+    driver.implicitly_wait(5)
     yield driver
     driver.quit()
 
